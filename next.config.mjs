6ce3382e527b94aae35b53e41b2d25d7c5a530c2
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-06-04 00:39:13 +0900

    set up config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+  reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
