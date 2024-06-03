16a9693b54a4d292607ed88853f1218b3c107e36
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-06-04 00:25:24 +0900

    set up shadcn config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>Hello, I'm AIT-Creators</>
+    <>
+      <>Hello, I'm AIT-Creators</>
+      <Button>Click me</Button>
+    </>
   );
 }
