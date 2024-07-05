87e06385f4df4266c094edd90b8a079f0cc74ba5
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-06 01:10:15 +0900

    set up github actions

--- next.config.mjs
+++ next.config.mjs
@@ -9,3 +9,23 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? `/${process.env.GITHUB_REF_NAME}` : "";
+
+module.exports = {
+  basePath: basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
+  exportTrailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
