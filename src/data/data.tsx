a0e664191b3cdcc9891a7c39a606a7a4d5a261af
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-28 22:17:46 +0900

    add: sample data

---
+++ src/data/data.tsx
@@ -0,0 +1,32 @@
+export const GAME = [
+  {
+    title: "Game Title",
+    description: "Game Description",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/game-1.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-2.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-3.jpg",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+    releaseDate: "2020-01-01",
+    trailer: "https://www.youtube.com/watch?v=videoId",
+    social: {
+      facebook: "https://www.facebook.com/game",
+      twitter: "https://www.twitter.com/game",
+      instagram: "https://www.instagram.com/game",
+      youtube: "https://www.youtube.com/game",
+    },
+  },
+];
