f59d9a4cd35b8aec8d3f0284df4875562f878e38
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-28 18:35:41 +0900

    add: games page header

---
+++ src/app/(pages)/games/_components/header/index.tsx
@@ -0,0 +1,11 @@
+import "@/src/styles/tailwind.css";
+
+export default function Page() {
+  return (
+    <div className="flex translate-y-0 items-end justify-center lg:translate-y-[-50px]">
+      <h1 className="offside relative z-10 flex text-[6rem] md:text-[8rem]">
+        <span className="neon_orange">GAMES</span>
+      </h1>
+    </div>
+  );
+}
--- src/app/(pages)/games/page.tsx
+++ src/app/(pages)/games/page.tsx
@@ -1,6 +1,9 @@
+import Header from "./_components/header";
+
 export default function Page() {
   return (
     <div>
+      <Header />
       <p>games</p>
     </div>
   );
