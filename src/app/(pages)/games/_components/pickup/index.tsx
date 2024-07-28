47f3a19cc5e596daaa21950b6672b032062771fa
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-28 22:45:25 +0900

    fix

--- src/app/(pages)/games/_components/pickup/index.tsx
+++ src/app/(pages)/games/_components/pickup/index.tsx
@@ -1,13 +1,9 @@
 export default function Page() {
   return (
-    <div>
-      <div>
-        <div className="flex translate-y-0 items-end justify-start lg:translate-y-[-50px]">
-          <h1 className="offside z-10 border-4 border-cyan-500 p-2 text-[4rem] md:text-[6rem]">
-            <span className="neon_blue">PICK UP !</span>
-          </h1>
-        </div>
-      </div>
+    <div className="flex translate-y-0 items-end justify-start lg:translate-y-[-50px]">
+      <h1 className="offside z-10 border-4 border-cyan-500 p-2 text-[4rem] md:text-[6rem]">
+        <span className="neon_blue">PICK UP !</span>
+      </h1>
     </div>
   );
 }
