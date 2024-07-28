d4b7338bc88ff1f96f8349de3b9d6e16e39e157e
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-28 23:13:03 +0900

    add: create card

--- src/app/(pages)/games/_components/pickup/index.tsx
+++ src/app/(pages)/games/_components/pickup/index.tsx
@@ -1,9 +1,33 @@
+import Image from "next/image";
+import { GAMES } from "@/src/data";
+
 export default function Page() {
   return (
-    <div className="flex translate-y-0 items-end justify-start lg:translate-y-[-50px]">
-      <h1 className="offside z-10 border-4 border-cyan-500 p-2 text-[4rem] md:text-[6rem]">
-        <span className="neon_blue">PICK UP !</span>
-      </h1>
-    </div>
+    <>
+      <div className="flex translate-y-0 items-end justify-start lg:translate-y-[-50px]">
+        <h1 className="offside z-10 border-4 border-cyan-500 p-2 text-[4rem] md:text-[6rem]">
+          <span className="neon_blue">PICK UP !</span>
+        </h1>
+      </div>
+      <div className="flex justify-around">
+        {GAMES.map((game) => {
+          if (game.pickup) {
+            return (
+              <div key={game.id} className="skew-x-[-15deg] border">
+                <Image
+                  alt={game.images[0].alt}
+                  height={200}
+                  src={game.images[0].src}
+                  width={200}
+                />
+                <h2 className="flex justify-center">{game.title}</h2>
+              </div>
+            );
+          }
+
+          return null;
+        })}
+      </div>
+    </>
   );
 }
---
+++ src/data/index.tsx
@@ -0,0 +1,126 @@
+export const GAMES = [
+  {
+    id: 1,
+    title: "Game Title1",
+    description: "Game Description",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/game-1.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-2.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-3.jpg",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+    releaseDate: "2020-01-01",
+    trailer: "https://www.youtube.com/watch?v=videoId",
+    social: {
+      facebook: "https://www.facebook.com/game",
+      twitter: "https://www.twitter.com/game",
+      instagram: "https://www.instagram.com/game",
+      youtube: "https://www.youtube.com/game",
+    },
+  },
+  {
+    id: 2,
+    title: "Game Title2",
+    description: "Game Description",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/game-1.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-2.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-3.jpg",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+    releaseDate: "2020-01-01",
+    trailer: "https://www.youtube.com/watch?v=videoId",
+    social: {
+      facebook: "https://www.facebook.com/game",
+      twitter: "https://www.twitter.com/game",
+      instagram: "https://www.instagram.com/game",
+      youtube: "https://www.youtube.com/game",
+    },
+  },
+  {
+    id: 3,
+    title: "Game Title3",
+    description: "Game Description",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/game-1.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-2.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-3.jpg",
+        alt: "Game Image",
+      },
+    ],
+    pickup: false,
+    releaseDate: "2020-01-01",
+    trailer: "https://www.youtube.com/watch?v=videoId",
+    social: {
+      facebook: "https://www.facebook.com/game",
+      twitter: "https://www.twitter.com/game",
+      instagram: "https://www.instagram.com/game",
+      youtube: "https://www.youtube.com/game",
+    },
+  },
+  {
+    id: 4,
+    title: "Game Title4",
+    description: "Game Description",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/game-1.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-2.jpg",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/game-3.jpg",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+    releaseDate: "2020-01-01",
+    trailer: "https://www.youtube.com/watch?v=videoId",
+    social: {
+      facebook: "https://www.facebook.com/game",
+      twitter: "https://www.twitter.com/game",
+      instagram: "https://www.instagram.com/game",
+      youtube: "https://www.youtube.com/game",
+    },
+  },
+];
