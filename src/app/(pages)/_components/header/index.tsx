1d6ea0a2f38b6d43aabaf03b74e5d9cfdd646a4d
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-08-02 14:12:08 +0900

    fix header size

--- src/app/(pages)/_components/header/index.tsx
+++ src/app/(pages)/_components/header/index.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="relative">
       <div className="flex items-end justify-start">
-        <h1 className="offside relative z-10 flex text-[3rem] md:text-[4rem]">
+        <h1 className="offside relative z-10 flex text-[2rem] md:text-[4rem]">
           <span className="neon_blue">BeCreate</span>
           <span className="neon_orange rotate-12">!</span>
         </h1>
