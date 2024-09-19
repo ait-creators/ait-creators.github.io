0bd773daa9431e68ce05a5a3100873b64d9d95f4
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-19 19:13:52 +0900

    fix card

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,19 +3,10 @@ import Carousel from "./_components/carousel";
 export default function Page() {
   return (
     <div>
-      <div>
+      <div className="translate-y-0 md:translate-y-0">
         <Carousel />
       </div>
       <div className="translate-y-[250px] md:translate-y-[500px]">
-        <Carousel />
-      </div>
-      <div className="translate-y-[500px] md:translate-y-[1000px]">
-        <Carousel />
-      </div>
-      <div className="block translate-y-[750px] md:hidden">
-        <Carousel />
-      </div>
-      <div className="block translate-y-[1000px] md:hidden">
         <Carousel />
       </div>
     </div>
