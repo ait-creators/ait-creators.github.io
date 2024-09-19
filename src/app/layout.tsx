e089805cdaf6fd48e04dfc4d0dae4309c50aed45
shouta0715 <Myheroes0715@gmail.com>
2024-09-19 19:51:36 +0900

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Metadata } from "next";
 import "@/src/styles/tailwind.css";
+import { Inter as FontSans } from "next/font/google";
 import React from "react";
+import { cn } from "@/src/lib/utils";
 
 export const metadata: Metadata = {
   title: "BeCreAIT",
   description: "BeCreAIT",
 };
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
@@ -14,9 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <head />
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
       <GoogleAnalytics gaId="G-EJLW92B540" />
     </html>
   );
