0c8be513b8e18e5dec8c04a2a6dbdf82d6a62b7e
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-19 23:27:15 +0900

    fix card size

--- src/app/(app)/_components/card/index.tsx
+++ src/app/(app)/_components/card/index.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ image }) => {
   return (
-    <div className="group relative flex h-[200px] min-w-[300px] skew-x-[-15deg] items-center justify-center overflow-hidden rounded-xl bg-slate-400 md:h-[400px] md:min-w-[600px]">
+    <div className="group relative flex h-[200px] min-w-[300px] skew-x-[-15deg] items-center justify-center overflow-hidden rounded-xl bg-slate-400 md:h-[350px] md:min-w-[600px]">
       <div className="hidden group-hover:block">
         <div className="absolute inset-0 z-10 flex items-center justify-center">
           <div className="pointer-events-none absolute size-full bg-black opacity-50" />
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -6,8 +6,16 @@ export default function Page() {
       <div className="translate-y-0 md:translate-y-0">
         <Carousel />
       </div>
-      <div className="translate-y-[250px] md:translate-y-[500px]">
+      <div className="translate-y-[230px] md:translate-y-[430px]">
         <Carousel />
+      </div>
+      <div className="translate-y-[460px] md:translate-y-[860px]">
+        <Carousel />
+      </div>
+      <div className="block md:hidden">
+        <div className="translate-y-[690px]">
+          <Carousel />
+        </div>
       </div>
     </div>
   );
