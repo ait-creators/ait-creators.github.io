4c0780cbab53a6f95ea12594275293b87babf900
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-20 00:59:19 +0900

    add: set data

--- src/app/(app)/_components/card/index.tsx
+++ src/app/(app)/_components/card/index.tsx
@@ -1,10 +1,7 @@
 import Image from "next/image";
+import { Game } from "@/src/lib/types";
 
-interface CardProps {
-  image: string;
-}
-
-const Card: React.FC<CardProps> = ({ image }) => {
+export default function Card({ game }: { game: Game }) {
   return (
     <div className="group relative flex h-[200px] min-w-[300px] skew-x-[-15deg] items-center justify-center overflow-hidden rounded-xl bg-slate-400 md:h-[350px] md:min-w-[600px]">
       <div className="hidden group-hover:block">
@@ -12,9 +9,12 @@ const Card: React.FC<CardProps> = ({ image }) => {
           <div className="pointer-events-none absolute size-full bg-black opacity-50" />
         </div>
       </div>
-      <Image alt={image} fill src={image} style={{ objectFit: "cover" }} />
+      <Image
+        alt={game.title}
+        height={600}
+        src={game.images[0].src}
+        width={600}
+      />
     </div>
   );
-};
-
-export default Card;
+}
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -3,126 +3,242 @@ import { Game } from "@/src/lib/types";
 export const GAMES: Game[] = [
   {
     id: 1,
-    title: "Game Title1",
-    description: "Game Description",
+    title: "MTuberへの道",
+    description:
+      "世はまさに大筋トレ時代！！時は2XXX年、筋肉を極めし者たちが、己の鍛え抜かれた肉体を誇示し、世界中にその力を発信する時代が到来！！筋肉の美しさと力強さを追求するMuscleTuber(通称MTuber)が爆発的に流行し、誰もがその頂点を目指すことを夢見ている！！「よし、やってやるぞ！！」僕は手に握りしめた腹筋ローラーを高々と掲げ、決意を胸にライブ開始ボタンを押した。大物MTuberに俺はなる！！",
     images: [
       {
         title: "Game Title",
-        src: "/games/game-1.jpg",
+        src: "/games/1/1.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-2.jpg",
+        src: "/games/1/2.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-3.jpg",
+        src: "/games/1/3.png",
         alt: "Game Image",
       },
     ],
     pickup: true,
-    releaseDate: "2020-01-01",
-    trailer: "https://www.youtube.com/watch?v=videoId",
-    social: {
-      facebook: "https://www.facebook.com/game",
-      twitter: "https://www.twitter.com/game",
-      instagram: "https://www.instagram.com/game",
-      youtube: "https://www.youtube.com/game",
-    },
   },
   {
     id: 2,
-    title: "Game Title2",
-    description: "Game Description",
+    title: "シューティス",
+    description:
+      "宇宙ステーションに隕石やUFOが接近！このままだと何かしらの被害を受ける可能性あり!砲台を用い、至急迎撃せよ!特徴・360度3Dシューティング・椅子とレバーを用いた直感的操作・没入感を上げる演出",
     images: [
       {
         title: "Game Title",
-        src: "/games/game-1.jpg",
+        src: "/games/2/1.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-2.jpg",
+        src: "/games/2/2.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-3.jpg",
+        src: "/games/2/3.png",
         alt: "Game Image",
       },
     ],
     pickup: true,
-    releaseDate: "2020-01-01",
-    trailer: "https://www.youtube.com/watch?v=videoId",
-    social: {
-      facebook: "https://www.facebook.com/game",
-      twitter: "https://www.twitter.com/game",
-      instagram: "https://www.instagram.com/game",
-      youtube: "https://www.youtube.com/game",
-    },
   },
   {
     id: 3,
-    title: "Game Title3",
-    description: "Game Description",
+    title: "シュレッター侍",
+    description:
+      "20XX年、急速な情報化が進んだ社会の中で書類が紙からデジタルへと変わっいった。しかしその中で情報漏洩のリスクも高まり、再び紙媒体への回帰が始まった。政府は機密情報などを保護し、確実に廃棄する方法を模索した。そこで政府は特別な役割を持つ侍を会社に派遣したのだ。そう、その名は「シュレッダー侍」！！特徴・刀を実際に振って切る斬撃アクション！・納刀で一気に破壊する映画的気持ちよさ！・正しい書類だけを切る緊張感！",
     images: [
       {
         title: "Game Title",
-        src: "/games/game-1.jpg",
+        src: "/games/3/1.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-2.jpg",
+        src: "/games/3/2.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-3.jpg",
+        src: "/games/3/3.png",
         alt: "Game Image",
       },
     ],
     pickup: false,
-    releaseDate: "2020-01-01",
-    trailer: "https://www.youtube.com/watch?v=videoId",
-    social: {
-      facebook: "https://www.facebook.com/game",
-      twitter: "https://www.twitter.com/game",
-      instagram: "https://www.instagram.com/game",
-      youtube: "https://www.youtube.com/game",
-    },
   },
   {
     id: 4,
-    title: "Game Title4",
-    description: "Game Description",
+    title: "とべ！！はなロケット",
+    description:
+      "宇宙中がスギの木まみれになってしまった！くしゃみエネルギーで動くロケット「NOSE」を政府に無許可で開発したあなたは宇宙中の全てのスギの木を薙ぎ倒すため他の惑星をぶっとばし、くしゃみエネルギーを集め始める...はなに指を突っ込んで機体を操作！！両指を突っ込んでチャージ！！両指を離してダッシュ！！がんばれ！はなロケット！！花粉症患者の希望の星となるのだ！！！",
     images: [
       {
         title: "Game Title",
-        src: "/games/game-1.jpg",
+        src: "/games/4/1.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-2.jpg",
+        src: "/games/4/2.png",
         alt: "Game Image",
       },
       {
         title: "Game Title",
-        src: "/games/game-3.jpg",
+        src: "/games/4/3.png",
         alt: "Game Image",
       },
     ],
     pickup: true,
-    releaseDate: "2020-01-01",
-    trailer: "https://www.youtube.com/watch?v=videoId",
-    social: {
-      facebook: "https://www.facebook.com/game",
-      twitter: "https://www.twitter.com/game",
-      instagram: "https://www.instagram.com/game",
-      youtube: "https://www.youtube.com/game",
-    },
+  },
+  {
+    id: 5,
+    title: "ピピっとバーコードマスター",
+    description:
+      "ピピっとバーコードマスターは、バーコードリーダーを使ってゲーム画面に出された商品の画像を見て、対応する実際の商品を読み込んで納品していくゲームです。バーコードを読み込んだ時の”ピッ！”という音を楽しんでもらえるようにバーコードを沢山読み込めるゲームにしました。特徴・バーコードリーダーで実際の商品を次々読み込んでいく。・納品には時間制限があるので、急がなければいけない。・荷台を持った人は対応していない商品を発注してくるので　その商品は取り扱っていないと教えてあげないといけない。",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/5/1.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/5/2.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/5/3.png",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+  },
+  {
+    id: 6,
+    title: "ムービーフェス",
+    description:
+      "あなたはハリウッドに出演した名俳優そして、主演映画が大ヒット！授賞式に出席するため映画祭へやってきた。華やかなレッドカーペットを歩きながらファンや記者にサインを求められたり、トロフィーを獲得するというチャレンジに挑む。時にはあなたがカメラマンに写真を撮られる場面も...！目指すは無事にゴール地点での授賞式に出演すること！しかし、カーペット上にはバナナの皮や爆弾も待ち受けている。最優秀賞を手にするのは一体誰だ！！！",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/6/1.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/6/2.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/6/3.png",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+  },
+  {
+    id: 7,
+    title: "ヨガアース",
+    description:
+      "荒廃し砂漠化した世界に芽を出した一本の木貴方はヨガの立木のポーズで木を成長させ、この世界に豊かな緑を取り戻そう！制限時間内にどれだけ木を伸ばせたかでスコアが決まります。プレイ内容・立木のポーズで木をどんどん成長させよう・障害物があるので避けて成長させようたくさんプレイすることでどんどん世界が豊かになる！この世界に緑を取り戻せ！",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/7/1.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/7/2.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/7/3.png",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+  },
+  {
+    id: 8,
+    title: "光の魔法使い",
+    description:
+      "アイドルになりたいモンスターたちがライブステージを奪いにきた？！推しのアイドルのため、モンスターを倒してステージを取り戻そう！",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/8/1.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/8/2.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/8/3.png",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+  },
+  {
+    id: 9,
+    title: "神様のおみくじ通り",
+    description:
+      "一月一日、とある神社では神様が困っていました。どうやら、参拝者が多過ぎて捌ききれていない様子。おみくじ売り場を担当して、神様のお手伝いをしましょう！制限時間内に何人の参拝者におみくじを渡せたかでスコアが決まります。プレイ内容1.神様の力で参拝者の望みを感知することができるので、参拝者ごとの望みを覚える2.おみくじを実際に引く3.適した運勢のおみくじが出たら対応するお客さんに渡す早くおみくじを渡すほど追加得点！たくさんの参拝者におみくじを渡して、高得点を目指しましょう！",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/9/1.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/9/2.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/9/3.png",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
+  },
+  {
+    id: 10,
+    title: "世界の運命はこの手の中に",
+    description:
+      "迫り来る隕石から地球を救え！ある日、ロボットが手だけとなってしまった。その手が地球の運命を握っている！トルマリン鉱石を集めてエネルギーを貯めて隕石を破壊せよ！地球を救えるか救えないかはあなた次第！世界の運命はこの手の中に！！！特徴・2Dゲーム・ロボットの手を操作してステージに落ちているトルマリン鉱石を集め、ゴールを目指します・あなた次第で結末が変わります",
+    images: [
+      {
+        title: "Game Title",
+        src: "/games/10/1.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/10/2.png",
+        alt: "Game Image",
+      },
+      {
+        title: "Game Title",
+        src: "/games/10/3.png",
+        alt: "Game Image",
+      },
+    ],
+    pickup: true,
   },
 ];
