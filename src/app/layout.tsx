781ac6f22e79562c4f7c2eef7036354b406fce56
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-20 13:11:45 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja" suppressHydrationWarning className="overflow-x-hidden">
+    <html className="overflow-x-hidden" lang="ja" suppressHydrationWarning>
       <head />
       <body
         className={cn(
