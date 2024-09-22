cf166224b422a0b0b02ec7778c19618f5e26f54c
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-23 01:26:29 +0900

    fix

--- src/app/(pages)/games/[slug]/_components/content/index.tsx
+++ src/app/(pages)/games/[slug]/_components/content/index.tsx
@@ -4,11 +4,17 @@ import { Game } from "@/src/lib/types";
 
 export default function Content({ game }: { game: Game }) {
   return (
-    <div className="mx-auto flex w-4/5 items-start justify-center pt-4 md:pt-0">
-      <div className="double-border w-full">
-        <h1 className="text-2xl font-black text-white">ゲーム説明</h1>
-        <p className="text-base md:text-3xl">{game.description}</p>
+    <>
+      <div className="mb-5 flex items-end justify-start py-1 md:py-5">
+        <h1 className="offside neon_border_orange border-4 border-orange-500 p-2 text-[1rem] font-bold md:text-[3rem]">
+          <span className="neon_orange">ゲーム説明</span>
+        </h1>
       </div>
-    </div>
+      <div className="mx-auto flex w-4/5 items-start justify-center pt-4 md:pt-0">
+        <div className="double-border w-full">
+          <p className="text-base md:text-3xl">{game.description}</p>
+        </div>
+      </div>
+    </>
   );
 }
